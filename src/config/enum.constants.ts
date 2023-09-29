bd2900701edcf922c7c72cf2019e1eee20f74ef5
nhatminhpham772 <nhatminh772002@gmail.com>
2023-09-29 16:34:08 +0700

    fix

--- src/config/enum.constants.ts
+++ src/config/enum.constants.ts
@@ -11,12 +11,3 @@ export enum Relationship{
     Other = 'Other'
 }
 
-export enum HealthStats {
-    HEIGHT = "height",
-    WEIGHT = "weight",
-    HEART_RATE = "heart_rate",
-    TEMPERATURE = "temperature",
-    BLOOD_PRESSURE = "blood_pressure",
-    WAIST_CIRCUMFERENCE = "waist_cricumference"
-}
-
