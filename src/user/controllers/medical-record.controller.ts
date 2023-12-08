358c3eab32795efcc81e71d084f646848972c964
nhatminhpham772 <nhatminh772002@gmail.com>
2023-12-08 15:11:03 +0700

    medical quantity

--- src/user/controllers/medical-record.controller.ts
+++ src/user/controllers/medical-record.controller.ts
@@ -108,13 +108,7 @@ export class MedicalRecordController {
     @ApiResponse({ status: 401, description: 'Chưa xác thực người dùng' })
     @Get('/statistical/quantity')
     async statisticalMedicalRecord(): Promise<any> {
-        // const cacheSchedules = await this.cacheManager.get('quantity' + this.medicalRecordService.VNTime().getFullYear() + this.medicalRecordService.VNTime().getMonth());
-        // if (cacheSchedules) return cacheSchedules
-
         const data = await this.medicalRecordService.statisticalMedicalRecord()
-
-        // await this.cacheManager.set('quantity' + this.medicalRecordService.VNTime().getFullYear() + this.medicalRecordService.VNTime().getMonth(), data)
-
         return data
     }
 }
